c2fbb208bb5abf5b529a0261efec93a9f41aaacf
Ikaika Akiona <ikaika.t.akiona@mssa.unitedtraining.com>
2021-09-16 20:04:09 -0400

    george

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -2,17 +2,28 @@
 using System.Collections.Generic;
 using System.Text;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
+using System.Linq;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
         // TODO - Wire up the CreatePrize for text files.
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            // load the text file convert the text to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+            // find the max id
+            int currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            model.Id = currentId;
+            // add the new record with the new id (max + 1)
+            prizes.Add(model);
+                        
+            // convert the prizes to a list<string>
+            // save a list<string> to the text file
 
-            return model;
         }
     }
 }
