37e379d565a7291953929f6869b508615457056f
Ikaika Akiona <ikaika.t.akiona@mssa.unitedtraining.com>
2021-09-20 00:39:59 -0400

    sql

--- TrackerLibrary/Models/MatchupEntryModel.cs
+++ TrackerLibrary/Models/MatchupEntryModel.cs
@@ -7,6 +7,10 @@ namespace TrackerLibrary.Models
     public class MatchupEntryModel
     {
         /// <summary>
+        /// The unique identifier for the prize
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
         /// Represents one team in the matchup
         /// </summary>
         public TeamModel TeamCompeting { get; set; }
